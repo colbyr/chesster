fefcb583a4c846aed87b9574caccebfd1b73badc
Dan Miller <dan@danielsmiller.com>
2012-11-29 13:34:40 -0500

    square model, piece model and modifications to bitboard model.

--- models/bitboard.rb
+++ models/bitboard.rb
@@ -1,5 +1,3 @@
-
-
 class Bitboard
   MaxValue = 0xffffffffffffffff
   MinValue = 0x0
@@ -16,7 +14,7 @@ class Bitboard
           1<<56, 1<<57, 1<<58, 1<<59, 1<<60, 1<<61, 1<<62, 1<<63]
 
   def initialize(value=0)
-    raise ArgumentError, sprintf("%32x", value) + " passed\nis out of range." if value < MinValue || value > MaxValue
+    #raise ArgumentError, sprintf("%32x", value) + " passed\nis out of range." if value < MinValue || value > MaxValue
     @value=value
   end
 
--- models/piece.rb
+++ models/piece.rb
@@ -1,12 +1,66 @@
+require './models/square.rb'
+require './models/bitboard.rb'
+
 class Piece
 
-  def initialize(color, name)
-    @color = color
-    @name = name
+  Type = [:king, :queen, :rook, :bishop, :knight, :pawn]
+  Color = [:black, :white]
+
+  attr_reader :bitboard
+
+  def initialize(occupied=[])
+    @bitboard = Bitboard.new
+    occupied.each{|p| set!(p)}
   end
 
-  def to_s
-    @color.to_s + @name.to_s
+  def ==(other)
+    @bitboard == other.bitboard
   end
 
+  # Bitwise and
+  def &(other)
+    make_piece @bitboard & other.bitboard
+  end
+
+  # Bitwise or
+  def |(other)
+    make piece @bitboard | other.bitboard
+  end
+
+  # Bitwise not
+  def ~()
+    Bitboard.new(~@bitboard)
+  end
+
+  # Bitwise xor
+  def ^(other)
+    make_piece @bitboard ^ other.bitboard
+  end
+
+  def set!(square)
+    @bitboard.set!(Square.position(square))
+  end
+
+  def clear!(square)
+    @bitboard.clear!(Square.position(square))
+  end
+
+  def set?(square)
+    @bitboard.set?(Square.position(square))
+  end
+
+  def clear?(square)
+    @bitboard.clear?(Square.position(square))
+  end
+
+  def set_bits
+    @bitboard.set_bits
+  end
+
+  private
+  def make_piece(bitboard)
+    new_piece = Piece.new
+    Square.each_value{|pos| new_piece.set!(Square.index(pos)) if bitboard.set?(pos)}
+    new_piece
+  end
 end
