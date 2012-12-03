580e614e6e8baa62f9d01a30849b2c04416bb71d
Dan Miller <dan@danielsmiller.com>
2012-12-03 16:33:43 -0500

    Chess server API is completely implemented with tests

--- lib/api.rb
+++ lib/api.rb
@@ -1,16 +1,25 @@
 require 'httparty'
+require 'json'
 
 class API
   include HTTParty
 
-  base_uri 'http://localhost:4567'
+  base_uri 'http://www.bencarle.com/chess'
 
   def status(options={})
-    self.class.get('/ready', options);
+    self.class.get('/ready', options)
   end
 
   def not_ready(options={})
-    self.class.get('/not_ready', options);
+    self.class.get('/not_ready', options)
+  end
+
+  def poll(game_id, team_number, team_secret)
+    JSON.parse(self.class.get("/poll/#{game_id}/#{team_number}/#{team_secret}/").parsed_response)
+  end
+
+  def move(game_id, team_number, team_secret, move_string)
+    JSON.parse(self.class.get("/move/#{game_id}/#{team_number}/#{team_secret}/#{move_string}/").parsed_response)
   end
 
 end
---
+++ specs/api_spec.rb
@@ -0,0 +1,33 @@
+require './lib/api.rb'
+
+describe API do
+ context 'it is not our turn' do
+   before(:each) do
+    @api = mock(API)
+    @api.stub!(:poll).and_return({'ready' => false, 'secondsleft' => 123.45, 'lastmovenumber' => 2})
+    @api.stub!(:move).and_return({'message' => '', 'result' => true})
+   end
+
+   it "should poll the server for a new move" do
+     response = @api.poll 
+     response['ready'].should eq(false)
+   end 
+
+   it "should be able to inform the server of a move" do
+     @response = @api.move(11,1,"32c68cae","Nb1c3")
+     @response['result'].should eq(true)
+   end
+ end
+
+ context 'it is our turn' do
+   before(:each) do
+    @api = mock(API)
+    @api.stub!(:poll).and_return({'ready' => true, 'secondsleft' => 123.45, 'lastmovenumber' => 3, 'lastmove' => 'Pd2d3'})
+   end
+
+   it "should poll the server for seconds left" do
+     response = @api.poll
+     response['secondsleft'].should eq(123.45)
+   end
+ end
+end
