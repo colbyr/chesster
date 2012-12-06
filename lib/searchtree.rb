218dbf02e0790397da8c86f1944f790e59ef1335
Colby Rabideau <crabideau5691@gmail.com>
2012-12-05 21:32:40 -0500

    reenabled some tests

--- lib/searchtree.rb
+++ lib/searchtree.rb
@@ -8,7 +8,7 @@ class SearchTree
   def initialize(state=generate)
     @color = :white
     @current_state = state
-    @depth = 3
+    @depth = 4
     @heuristic_bound = 1000000
     @nodes_visited = 0
   end
@@ -48,15 +48,10 @@ class SearchTree
     # TODO Detect check/end of game
     if depth > 0
       alpha = @heuristic_bound * -player
-      next_player = player * -1
       for move in Move.new(position).gen_all_moves(get_color(player))
         @nodes_visited += 1
-        test = minimax(position.move(move), depth-1, next_player)
-        opts = [alpha, test]
-        alpha = (player == 1) ? opts.max : opts.min
-        if depth == 100
-          # puts (player == 1 ? 'max' : 'min') + ': from ' + opts.to_s + ' chose ' + alpha.to_s
-        end
+        test = minimax(position.move(move), depth-1, -player)
+        alpha = (player == 1) ? [alpha, test].max : [alpha, test].min
       end
     else
       alpha = heuristic position
@@ -64,18 +59,15 @@ class SearchTree
     return alpha
   end
 
-  def search(position, player=1)
+  def search(position, depth=@depth, player=1)
     raise 'player must equal 1 or -1' if player.abs != 1
     res = [@heuristic_bound * player, nil]
     Move.new(position).gen_all_moves(get_color(player)).each {|move|
       pos = position.move(move)
-      alpha = minimax(pos, @depth - 1, -player)
+      alpha = minimax(pos, depth - 1, -player)
       test = [alpha, move]
       res = res[0] < test[0] ? res : test
     }
-    puts
-    puts 'alpha: ' + res[0].to_s
-    puts 'nodes: ' + @nodes_visited.to_s
     return res[1]
   end
 
