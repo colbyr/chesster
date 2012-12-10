98ded77d5ea3a599bb43e081f8491b27ac9fffa3
Colby Rabideau <idontknow5691@gmail.com>
2012-12-09 21:14:46 -0500

    remote starter

---
+++ remote.rb
@@ -0,0 +1,38 @@
+require './lib/chesster.rb'
+require 'celluloid'
+
+puts 'Welcome to...'
+
+puts '   _____ _                   _            _                     _ '
+puts '  / ____| |                 | |          | |                   | |'
+puts ' | |    | |__   ___  ___ ___| |_ ___ _ __| |     ___   ___ __ _| |'
+puts ' | |    | \'_ \ / _ \/ __/ __| __/ _ \ \'__| |    / _ \ / __/ _` | |'
+puts ' | |____| | | |  __/\__ \__ \ ||  __/ |  | |___| (_) | (_| (_| | |'
+puts '  \_____|_| |_|\___||___/___/\__\___|_|  |______\___/ \___\__,_|_|'
+puts
+puts
+
+puts 'Go to http://www.bencarle.com/chess/startgame to start a game'
+puts 'Then supply the information asked for below'
+
+print 'Please enter the game id: '
+game_id = gets.chomp.to_i
+puts
+
+print 'Please enter chessters team number: '
+chesster_team_id = gets.chomp.to_i
+puts
+
+print 'What is chesters team secret?'
+chesster_secret = gets.chomp
+
+print 'Whats your color?'
+my_color = gets.chomp.to_sym
+puts
+
+puts 'Please and thank you.'
+
+chesster = Chesster.new(game_id, chesster_team_id, chesster_secret, my_color)
+
+puts 'The state currently is:'
+puts chesster.state.current_position.to_s
