60f11a86732b8fdf8f7d1ffb4bee38b0f571dac7
Colby Rabideau <idontknow5691@gmail.com>
2013-05-01 21:49:20 -0400

    :P

--- lib/chesster.rb
+++ lib/chesster.rb
@@ -16,7 +16,7 @@ class Chesster < API
     @searcher = SearchTree.new(color)
 
     puts 'Hi, my name is Chesster. I\'m the besster.'
-    puts 'hahahahahahahahahahahahahahahahha =)'
+    puts 'blahblahblah =('
     puts 'weiropaweifhsjkfhgjksdhfgs'
 
 
